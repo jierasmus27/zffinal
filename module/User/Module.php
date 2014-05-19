6ba7e5ee039920974b065c3041eb4b6c345ec780
Jaco Erasmus <jacoe@example.com>
2014-05-19 10:50:10 +0200

    initial commit

---
+++ module/User/Module.php
@@ -0,0 +1,48 @@
+<?php
+namespace User;
+
+class Module {
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+    
+    public function getServiceConfig() {
+        return array(
+            'factories' => array(
+                'Generic\Model\UserTable' => function($sm) {
+                    $tableGateway = $sm->get('UserTableGateway');
+                    $table = new AlbumTable($tableGateway);
+                    return $table;
+                },
+                'UserTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get(
+                            'Zend\Db\Adapter\Adapter'
+                    );
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(
+                            new User()
+                    );
+                    return new TableGateway(
+                            'user', $dbAdapter, null, $resultSetPrototype
+                    );
+                },
+            )
+        );
+    }
+}
+?>
