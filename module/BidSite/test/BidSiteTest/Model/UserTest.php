25f5a4e7a7babeb1264d4f2d9875efe6df5871b0
Jaco Erasmus <jierasmus27@gmail.com>
2014-06-01 16:52:42 +0200

    Added new UserTest

--- module/BidSite/test/BidSiteTest/Model/UserTest.php
+++ module/BidSite/test/BidSiteTest/Model/UserTest.php
@@ -1,11 +1,14 @@
 <?php
-namespace BidSiteTest;
+namespace BidSiteTest\Model;
 
 use BidSite\Model\User;
 use PHPUnit_Framework_TestCase;
 
 class UserTest extends PHPUnit_Framework_TestCase {
     
+public function testInitialState() {
+        $this->assertEquals(null, null); 
+    }
 }
 
 
